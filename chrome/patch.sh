720e1f141aa6b3d4e8de06929dab9da4162a5289
Laurelai <Laurelai@oneechan.org>
2011-07-15 23:42:57 -0400

    first commit

---
+++ chrome/patch.sh
@@ -0,0 +1,12 @@
+#/usr/bin/bash
+patch -p0 < chromium-c99math.patch
+patch -p0 < chromium-dlfcn.patch
+patch -p0 < chromium-futimens.patch
+patch -p0 < chromium-no-mit-screensaver.patch
+patch -p0 < chromium-stdstring.patch
+patch -p0 < chromium-no-sbrk.patch
+patch -p0 < chromium-tcmalloc.patch
+patch -p0 < chromium-uclibc-resolv.patch
+patch -p0 < chromium-yasm.patch
+patch -p0 < chromium-libpng15.patch
+patch -p0 < chromium-zlibheaders.patch
